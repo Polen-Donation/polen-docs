2ebaf996eff3a50daa6bd8b5b0b92f1d0f5291db
jeovane16 <jeovanearaujo16@gmail.com>
2021-02-24 17:50:54 -0300

    ajustes de link na configuração

--- polen-dev/docusaurus.config.js
+++ polen-dev/docusaurus.config.js
@@ -127,7 +127,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/Polen-Donation/polen-docs/pulls',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
