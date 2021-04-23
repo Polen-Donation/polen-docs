9768e357f3d25c6eedd8f8de7ee0631194e3818e
jeovane16 <jeovane.araujo@opolen.com.br>
2021-04-23 09:59:03 -0300

    Deploy website - based on a4c3d029e274d863369c3ff6694efc6d8bcaf13e

---
+++ 2d5692df.69a80766.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{81:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/polen-docs/blog/welcome"},{"title":"Hello","permalink":"/polen-docs/blog/hello-world"},{"title":"Hola","permalink":"/polen-docs/blog/hola"}]}')}}]);
---
+++ 8c74018e.8de1b1e2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{103:function(s){s.exports=JSON.parse('{"allTagsPath":"/polen-docs/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/polen-docs/blog/tags/docusaurus"}')}}]);
